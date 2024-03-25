daaa3b36cdc792ea9be527b7ce6e61604fff529e
Marco Brunetti <151136910+marcocodd@users.noreply.github.com>
2024-03-25 16:00:39 +0100

    Creata cartelle enums, dao Classe abbonamento (commentata) Emittente (commentata)

---
+++ src/main/java/team5/entities/Emittente.java
@@ -0,0 +1,23 @@
+package team5.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+
+//@Entity
+//public abstract class Emittente {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private long id;
+//    private String indirizzo;
+//
+//    public Emittente( String indirizzo) {
+//
+//        this.indirizzo = indirizzo;
+//    }
+//
+//    public Emittente() {
+//    }
+//}
---
+++ src/main/java/team5/enums/TipoAbbonamento.java
@@ -0,0 +1,6 @@
+package team5.enums;
+
+public enum TipoAbbonamento {
+    MENSILE,
+    SETTIMANALE
+}
