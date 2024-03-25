70474f9f71c67c619b137f515279a5e93d52dc4e
Riccardo Marchetti <marchettiriccardo04@gmail.com>
2024-03-25 17:15:36 +0100

    Biglietto e Rivenditore + dao

--- src/main/java/team5/entities/Rivenditore.java
+++ src/main/java/team5/entities/Rivenditore.java
@@ -1,4 +1,19 @@
 package team5.entities;
 
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Rivenditore {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private long id;
+    private String nome;
+    private long numeroTelefono;
+//    @OneToOne
+//    @JoinColumn (name = "emittente_id")
+    @OneToMany (mappedBy = "rivenditore")
+    private List<Biglietto> biglietto = new ArrayList<>();
 }
