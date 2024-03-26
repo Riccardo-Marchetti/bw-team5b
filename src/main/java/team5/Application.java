a8a21d07b3c4340b3e0cddbcbbc252a9e98ab1a4
AlessiaLuongo <151174462+AlessiaLuongo@users.noreply.github.com>
2024-03-26 09:55:32 +0100

    work in progress

--- src/main/java/team5/Application.java
+++ src/main/java/team5/Application.java
@@ -1,17 +1,31 @@
 package team5;
 
+import com.github.javafaker.Faker;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
+import team5.dao.TesseraDAO;
+import team5.dao.UtenteDAO;
 import team5.entities.Tessera;
 import team5.entities.Utente;
 
 import java.time.LocalDate;
+import java.util.Locale;
+import java.util.function.Supplier;
 
 public class Application {
     private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("gestionetrasporti");
     public static void main(String[] args) {
 
+        EntityManager em = emf.createEntityManager();
+        UtenteDAO utenteDAO = new UtenteDAO(em);
+        TesseraDAO tesseraDAO = new TesseraDAO(em);
+
+
+        Supplier<Utente> utenteSupplier = ()->{
+            Faker faker = new Faker(Locale.ITALY);
+            return new Utente(faker.rickAndMorty().character())
+        }
 
 
 
